293660827a419aa423d8afbaf447e42bcbad29f8
mloardXD <mloard2011@hotmail.com>
2024-06-16 12:33:23 -0400

    f

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -2,26 +2,27 @@ import requests
 import json
 
 def emotion_detector(text_to_analyse):
-   url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
-   myobj = { "raw_document": { "text": text_to_analyse } }
-   header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    myobj = { "raw_document": { "text": text_to_analyse } }
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
 
-   response = requests.post(url, json = myobj, headers=header)
-   formatted_response = json.loads(response.text)
-
-   emotions = {}
-   emotions['anger'] = formatted_response['emotionPredictions'][0]['emotion']['anger']
-   emotions['disgust'] = formatted_response['emotionPredictions'][0]['emotion']['disgust']
-   emotions['fear'] = formatted_response['emotionPredictions'][0]['emotion']['fear']
-   emotions['joy'] = formatted_response['emotionPredictions'][0]['emotion']['joy']
-   emotions['sadness'] = formatted_response['emotionPredictions'][0]['emotion']['sadness']
-
-   for emotion in emotions:
-      if emotions[emotion] == max(emotions.values()):
-         dominant_emotion = emotion     
-   emotions['dominant_emotion'] = dominant_emotion
-   return(emotions)
-    # 'dominant_emotion': dominant_emotion, 
+    response = requests.post(url, json = myobj, headers=header)
+    formatted_response = json.loads(response.text)
     
+    if response.status_code == 200:
+        emotions = {}
+        emotions['anger'] = formatted_response['emotionPredictions'][0]['emotion']['anger']
+        emotions['disgust'] = formatted_response['emotionPredictions'][0]['emotion']['disgust']
+        emotions['fear'] = formatted_response['emotionPredictions'][0]['emotion']['fear']
+        emotions['joy'] = formatted_response['emotionPredictions'][0]['emotion']['joy']
+        emotions['sadness'] = formatted_response['emotionPredictions'][0]['emotion']['sadness']
 
-# emotion_detector("I dont really like this, but am very disgusted")
+        for emotion in emotions:
+            if emotions[emotion] == max(emotions.values()):
+                dominant_emotion = emotion     
+        emotions['dominant_emotion'] = dominant_emotion
+        return(emotions)
+
+    elif response.status_code == 400:
+        return {'anger': None, 'disgust': None, 'fear': None, 'joy': None, 'sadness': None, 'dominant_emotion': None}
+    
--- server.py
+++ server.py
@@ -1,10 +1,17 @@
+"""Executing this function initiates the application of emotion detection
+   to be executed over the Flask channel and deployed on
+   localhost:5000."""
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
-def emotDetector():
+def emot_detector():
+    """This code receives the text from the HTML interface and 
+        runs emotion detection over it using emotion_detector()
+        function."""
+
     text_to_analyze = request.args.get('textToAnalyze')
     emotion = emotion_detector(text_to_analyze)
     anger = emotion["anger"]
@@ -13,16 +20,18 @@ def emotDetector():
     joy = emotion["joy"]
     sadness = emotion["sadness"]
     dominant_emotion = emotion["dominant_emotion"]
-    return "For the given statement, the system response is 'anger': {}, 'disgust': {}, 'fear': {}, 'joy': {} and 'sadness': {}. The dominant emotion is {}.".format(anger, disgust, fear, joy, sadness, dominant_emotion)
+
+    if dominant_emotion is None:
+        return "Invalid text! Please try again!."
+
+    return f"For the given statement, the system response is 'anger': {anger}, \
+    'disgust': {disgust}, 'fear': {fear}, 'joy': {joy} and 'sadness': {sadness}.\
+    The dominant emotion is {dominant_emotion}."
 
 @app.route("/")
 def render_index_page():
-    ''' This function initiates the rendering of the main application
-        page over the Flask channel
-    '''
+    """Renders index page"""
     return render_template('index.html')
 
 if __name__ == "__main__":
-    ''' This functions executes the flask app and deploys it on localhost:5000
-    '''
     app.run(host="0.0.0.0", port=5000)
